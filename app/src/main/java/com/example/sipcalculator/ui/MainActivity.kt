cda6865bc3fa483deee500279c226f880161a9f2
Utkarsh Rastogi <utkarsh.013@gmail.com>
2021-05-20 01:13:05 +0530

    Setting status bar color

--- app/src/main/java/com/example/sipcalculator/ui/MainActivity.kt
+++ app/src/main/java/com/example/sipcalculator/ui/MainActivity.kt
@@ -6,9 +6,23 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.example.sipcalculator.R
 import com.example.sipcalculator.ui.fragments.SipInputFragment
 
+import androidx.core.content.ContextCompat
+
+import android.view.WindowManager
+
+import android.view.Window
+
+
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.teal_800)
+
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
